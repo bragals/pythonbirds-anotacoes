89cb6e766df8699894c4e48bc83fa0deaab20a12
Lucas Braga <lucas_santos_braga@hotmail.com>
2019-08-04 17:27:10 -0400

    Exercício contagem caracteres - Lista

--- IDE_Pro/matematica/base/mat.py
+++ IDE_Pro/matematica/base/mat.py
@@ -1,6 +1,20 @@
+""" Módulo que contém operações matemáticas """
+
 def soma(parcela, parcela2):
+    """ Essa função calcula a soma de duas parcelas
+
+    :param parcela: number
+    :param parcela2: number
+    :return: number
+    """
     return parcela + parcela2
 
 
 if __name__ == '__main__':
-    print(soma(1, 2))
+    # Testando a função soma
+    print(soma(1, 2)) # Imprimindo...
+
+
+# Utilizar Crtl + / para comentar a seleção de texto
+# >>> help(mat.soma)
+# >>> mat.soma.__doc__
